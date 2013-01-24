cc9b2f541cf89909c2fdf1f8e694164a5720e8f5
Paul Butcher <paul@paulbutcher.com>
2013-01-24 11:19:43 +0000

    More helpful names

--- project/Build.scala
+++ project/Build.scala
@@ -14,31 +14,31 @@ object WordCountBuild extends Build {
     "core", 
     file("core"),
     settings = buildSettings ++ Seq(
-      name := "Utils", 
+      name := "core", 
       libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.1.0"))
 
   lazy val producer_pushes_bounded_queue = Project(
     "producer_pushes_bounded_queue", 
     file("producer_pushes_bounded_queue"),
-    settings = buildSettings ++ Seq(name := "Producer Pushes Bounded Queue")) dependsOn(core)
+    settings = buildSettings ++ Seq(name := "producer_pushes_bounded_queue")) dependsOn(core)
 
   lazy val producer_pushes_unbounded_queue = Project(
     "producer_pushes_unbounded_queue", 
     file("producer_pushes_unbounded_queue"),
-    settings = buildSettings ++ Seq(name := "Producer Pushes Unbounded Queue")) dependsOn(core)
+    settings = buildSettings ++ Seq(name := "producer_pushes_unbounded_queue")) dependsOn(core)
 
   lazy val consumer_pulls = Project(
     "consumer_pulls", 
     file("consumer_pulls"),
-    settings = buildSettings ++ Seq(name := "Consumer Pulls")) dependsOn(core)
+    settings = buildSettings ++ Seq(name := "consumer_pulls")) dependsOn(core)
 
   lazy val consumer_pulls_batched = Project(
     "consumer_pulls_batched", 
     file("consumer_pulls_batched"),
-    settings = buildSettings ++ Seq(name := "Consumer Pulls Batched")) dependsOn(core)
+    settings = buildSettings ++ Seq(name := "consumer_pulls_batched")) dependsOn(core)
 
   lazy val consumer_pulls_cached = Project(
     "consumer_pulls_cached", 
     file("consumer_pulls_cached"),
-    settings = buildSettings ++ Seq(name := "Consumer Pulls Cached")) dependsOn(core)
+    settings = buildSettings ++ Seq(name := "consumer_pulls_cached")) dependsOn(core)
 }
