6568290981ce91c7e5a8381b7c919ad2103c5a1d
Paul Butcher <paul@paulbutcher.com>
2013-01-24 11:16:41 +0000

    Configure RoundRobinRouter via code, not config

--- producer_pushes_bounded_queue/src/main/scala/com/paulbutcher/Master.scala
+++ producer_pushes_bounded_queue/src/main/scala/com/paulbutcher/Master.scala
@@ -1,12 +1,12 @@
 package com.paulbutcher
 
 import akka.actor._
-import akka.routing.{FromConfig, RoundRobinRouter}
+import akka.routing.RoundRobinRouter
 
 class Master extends Actor {
 
   val consumers = context.actorOf(Props[Consumer].
-    withRouter(FromConfig()).
+    withRouter(RoundRobinRouter(5)).
     withDispatcher("consumer-dispatcher"), "consumers")
   val producer = context.actorOf(Props(new Producer(consumers)), "producer")
 
